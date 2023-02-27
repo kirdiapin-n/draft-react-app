e919dbedd927294f51a7a5b081b81329b4e23ff6
mrblond1n <mrblond1n@yandex.ru>
2023-02-27 13:58:55 +0100

    try to use env variables

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>{process.env.TEST}</code> and save to reload.
+          Edit <code>{process.env.REACT_APP_TEST}</code> and save to reload.
         </p>
         <a
           className="App-link"
