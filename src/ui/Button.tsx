fddcdd49bbb478807dac67a6985285a9dd73a452
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-13 16:09:02 +0100

    branch commit #1

---
+++ src/ui/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Button = ({children, ...props}: React.ButtonHTMLAttributes<HTMLButtonElement>) =>
+    <button {...props}>
+        {children}
+    </button>
