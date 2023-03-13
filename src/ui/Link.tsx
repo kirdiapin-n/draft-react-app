63a0fbe5979e0feb9fd362d7873e649575f05c88
Nikita Kirdiapin <kirdiapin@alarstudios.com>
2023-03-13 16:15:17 +0100

    branch commit #4

--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 export const Link = ({children, ...props}: React.LinkHTMLAttributes<HTMLAnchorElement>) =>
-    <a {...props}>
+    <a style={style} {...props}>
         {children}
     </a>
+
+const style = {
+    color: 'white',
+    textDecoration: 'none',
+}
